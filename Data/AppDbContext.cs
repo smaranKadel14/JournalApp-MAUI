f621053259fee2248d374b0caab21725723a73e6
Smaran Kadel <kadelsmaran45@gmail.com>
2026-01-23 23:32:25 +0545

    Changed the logic of postgreSQL to SQLite in Login.razor & Register.razor

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -2,10 +2,10 @@
 using JournalApp.Models;
 
 namespace JournalApp.Data;
-
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
+    public DbSet<User> Users => Set<User>();
     public DbSet<JournalEntry> JournalEntries => Set<JournalEntry>();
 }
